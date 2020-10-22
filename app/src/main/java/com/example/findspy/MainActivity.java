1102f445de0182c2e00e129f5f64ab91d7b3b572
Hatef Zamany <hatef.zamany@yahoo.com>
2020-10-22 00:33:04 -0700

    control valid numbers

--- app/src/main/java/com/example/findspy/MainActivity.java
+++ app/src/main/java/com/example/findspy/MainActivity.java
@@ -23,18 +23,28 @@ public class MainActivity extends AppCompatActivity {
     public void decreasePlayer(View view){
         Button button = (Button) findViewById(R.id.playerNumber);
         int playerNumber = Integer.parseInt((String) button.getText()) - 1;
+        if(playerNumber < 1){
+            return;
+        }
         button.setText(String.valueOf(playerNumber));
     }
 
     public void increaseSpy(View view){
         Button button = (Button) findViewById(R.id.spyNumber);
+        Button playerButton = (Button) findViewById(R.id.playerNumber);
         int spyNumber = Integer.parseInt((String) button.getText()) + 1;
+        if(spyNumber > Integer.parseInt((String) playerButton.getText())){
+            return;
+        }
         button.setText(String.valueOf(spyNumber));
     }
 
     public void decreaseSpy(View view){
         Button button = (Button) findViewById(R.id.spyNumber);
         int spyNumber = Integer.parseInt((String) button.getText()) - 1;
+        if(spyNumber < 1){
+            return;
+        }
         button.setText(String.valueOf(spyNumber));
     }
 
