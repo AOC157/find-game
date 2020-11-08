8917f2b180d90772d67e4781cde4d05484c400ea
Hatef Zamany <hatef.zamany@yahoo.com>
2020-11-08 12:43:32 -0800

    add joker swith

--- app/src/main/java/com/example/findspy/StartActivity1.java
+++ app/src/main/java/com/example/findspy/StartActivity1.java
@@ -7,17 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 public class StartActivity1 extends AppCompatActivity {
 
     Spinner timer;
-
+    Switch joker;
+    boolean jokerBool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start1);
+        jokerBool = false;
         setTimer();
+        setJoker();
+    }
+
+    private void setJoker() {
+        joker.setChecked(false);
+        joker = (Switch) findViewById(R.id.joker);
+        joker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                jokerBool = !jokerBool;
+            }
+        });
     }
 
     private void setTimer() {
