6040ca36b34a389f2fd54ec2b058ccb1e8703a82
Hatef Zamany <hatef.zamany@yahoo.com>
2020-11-25 07:10:14 -0800

    cut intro music

--- app/src/main/java/com/example/findspy/MainActivity.java
+++ app/src/main/java/com/example/findspy/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.findspy;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
-import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
