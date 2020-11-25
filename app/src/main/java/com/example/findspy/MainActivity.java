8d0561069064b01ccff772a3cfbf3a7c3b646179
Hatef Zamany <hatef.zamany@yahoo.com>
2020-11-25 09:58:30 -0800

    set fade_in at start of app

--- app/src/main/java/com/example/findspy/MainActivity.java
+++ app/src/main/java/com/example/findspy/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        overridePendingTransition(R.anim.fade_in,R.anim.slide_out_left);
         playIntroMusic();
         startMainActivity();
     }
