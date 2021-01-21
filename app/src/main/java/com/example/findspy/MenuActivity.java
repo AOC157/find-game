4fafbafdf79acd18f3359c1c350e3cfaf85c11c1
Hatef Zamany <hatef.zamany@yahoo.com>
2021-01-21 16:31:09 +0330

    start StartActivity after choose mode

--- app/src/main/java/com/example/findspy/MenuActivity.java
+++ app/src/main/java/com/example/findspy/MenuActivity.java
@@ -2,34 +2,41 @@ package com.example.findspy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
-    Button food = findViewById(R.id.food);
-    Button thing = findViewById(R.id.thing);
-    Button place = findViewById(R.id.thing);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         setContentView(R.layout.activity_menu);
     }
 
     public void setPlaceMode(View view){
         StartActivity1.mode = "place";
         RunActivity.mode = "place";
+        Intent intent = new Intent(MenuActivity.this,StartActivity1.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.fade_in,R.anim.slide_out_left);
     }
 
     public void setThingMode(View view){
         StartActivity1.mode = "thing";
         RunActivity.mode = "thing";
+        Intent intent = new Intent(MenuActivity.this,StartActivity1.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.fade_in,R.anim.slide_out_left);
     }
 
     public void setFoodMode(View view){
         StartActivity1.mode = "food";
         RunActivity.mode = "food";
+        Intent intent = new Intent(MenuActivity.this,StartActivity1.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.fade_in,R.anim.slide_out_left);
     }
 }
