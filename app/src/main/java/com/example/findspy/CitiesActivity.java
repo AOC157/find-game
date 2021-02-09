9ecd14a75e66ef73b996f1ddc15489d5138984c5
Hatef Zamany <hatef.zamany@yahoo.com>
2021-02-09 23:06:41 +0330

    override finish

--- app/src/main/java/com/example/findspy/CitiesActivity.java
+++ app/src/main/java/com/example/findspy/CitiesActivity.java
@@ -11,4 +11,10 @@ public class CitiesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
     }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_down,R.anim.slide_out_up);
+    }
 }
